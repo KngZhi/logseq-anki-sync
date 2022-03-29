4f45bc7ad745d6de827ea137931551a298a502d6
KoKo <kngzhi@hotmail.com>
2022-03-29 13:03:39 +0800

    feat: get blocks by tag

--- src/query.ts
+++ src/query.ts
@@ -1,8 +1,9 @@
 import '@logseq/libs';
 import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user';
 import { map, get } from "lodash";
+import { Block } from './types';
 
-export async function getBlockByTag(tag: string) {
+export async function getBlocksByTag(tag: string): Block[] {
   const blocks = (await logseq.DB.datascriptQuery(`
             [:find (pull ?b [*])
             :where
@@ -11,34 +12,33 @@ export async function getBlockByTag(tag: string) {
             `))
     .flat()
 
-  /**
-   * it would do following things
-   * 1. get pageEntity
-   * 2. extract tags with given
-   */
-  async function transformBlock(block: BlockEntity) {
-    const uuid = block.uuid["$uuid$"]
-    const { Editor: { getBlock, getPage, } } = logseq
+  const result = await Promise.all(map(blocks, completeBlock))
+  return result
+}
 
-    block = {
-        ...block,
-        ...(await getBlock(uuid, { includeChildren: true })),
-    }
+/**
+* it would do following things
+* 1. get pageEntity
+* 2. extract tags with given
+*/
+async function completeBlock(block: BlockEntity): Promise<Block>{
+  const uuid = block.uuid["$uuid$"]
+  const { Editor: { getBlock, getPage }, } = logseq
 
-    block.pageContent = block.page
-      ? (await getPage(block.page.id))
-      : {}
-
-    const { refs = [] } = block
-    const tags = await Promise.all(map(
-      refs,
-      async page => get(await getPage(page.id), 'name')))
-    block.tags = tags || []
-
-    // const { children } = block
-    // block.children = await Promise.all(map(children, transformBlock)) || children
-
-    return block
+  block = {
+    ...block,
+    ...(await getBlock(uuid, { includeChildren: true })),
   }
-  return Promise.all(map(blocks, transformBlock))
+
+  block.pageContent = block.page
+    ? (await getPage(block.page.id))
+    : {}
+
+  const { refs = [] } = block
+  const tags = await Promise.all(map(
+    refs,
+    async page => get(await getPage(page.id), 'name')))
+  block.tags = tags || []
+
+  return block
 }
